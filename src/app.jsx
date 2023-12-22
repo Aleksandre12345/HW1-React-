2b899ac3ba609066a3f430ab91b3039f9201b377
Aleksandre <grdzelishvilisandro@gmail.com>
2023-12-22 21:57:13 +0400

    first commit

---
+++ src/app.jsx
@@ -0,0 +1,16 @@
+import './app.css'
+
+export function App() {
+
+  return (
+    <>
+    <div className='Container'>
+      <div className="bluediv">
+        <img className='qr' src="assets/qr-code.svg"/>
+      </div>
+      <h1>Improve your front-end skills by building projects</h1>
+      <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+    </div>
+    </>
+  )
+}
